42178a7d6e90736178f0d170e9d22576c0087143
Arnandis <arnandispau04@gmail.com>
2025-03-26 16:11:06 +0100

    Nou model Finanzas

--- src/models/Finanzas.js
+++ src/models/Finanzas.js
@@ -1,24 +1,3 @@
-/*const mongoose = require("mongoose");
-//Auth Fet
-//nia q cambiar el model per a que arreplegue els datos be
-const finanzasSchema = new mongoose.Schema({
-    usuarioId: { type: mongoose.Schema.Types.ObjectId, ref: "Usuario", required: true },
-    tipo: { type: String, enum: ["ingreso", "gasto"], required: true },
-    cantidad: { type: Number, required: true },
-    categoria: { 
-        type: String, 
-        enum: ["Ocio", "Alquiler", "Compras", "Festivales", "Juegos", "Salario", "Otros"], 
-        required: true 
-    },
-    fecha: { type: Date, default: Date.now },
-    descripcion: { type: String, default: "" }
-});
-
-const Finanzas = mongoose.model("Finanzas", finanzasSchema);
-
-module.exports = Finanzas;*/
-
-//este model alom esmillor pero no me ha funcionat
 const mongoose = require("mongoose");
 
 // Ajustamos el esquema para aceptar un objeto de gastos con múltiples categorías
